fc17cac58bbbde36af37d43f329fc92500484674
shaxweb <igrayunagitare@gmail.com>
2025-07-01 01:43:38 +0500

    update

--- SomeStore/main/urls.py
+++ SomeStore/main/urls.py
@@ -5,10 +5,10 @@ app_name = "main"
 
 urlpatterns = [
     path('', views.StartAPi.as_view()),
-    path('get_all_datas/', views.GetAllDatasApi.as_view()),
+    path('get_all_datas', views.GetAllDatasApi.as_view()),
     path('get_data', views.GetDataApi.as_view()),
-    path('register/', views.RegisterUserApi.as_view()),
-    path('auth/', views.AuthUserApi.as_view()),
-    path('create_product/', views.CreateProductApi.as_view()),
-    path('create_basket/', views.CreateBasketApi.as_view()),
+    path('register', views.RegisterUserApi.as_view()),
+    path('auth', views.AuthUserApi.as_view()),
+    path('create_product', views.CreateProductApi.as_view()),
+    path('create_basket', views.CreateBasketApi.as_view()),
 ]
