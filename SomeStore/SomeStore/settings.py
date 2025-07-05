2c4322894f84b085de698f05abd239fc63f9c2f1
shaxweb <igrayunagitare@gmail.com>
2025-07-05 14:08:52 +0500

    update

--- SomeStore/SomeStore/settings.py
+++ SomeStore/SomeStore/settings.py
@@ -33,6 +33,8 @@ ALLOWED_SLASH = True
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000"]
+CORS_ALLOW_METHODS = ["GET", "POST", "OPTIONS"]
+CORS_ALLOW_HEADERS = ["content-type", "authorization", "x-requested-with"]
 
 
 # Application definition
