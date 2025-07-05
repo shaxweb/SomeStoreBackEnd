2bc221ec1462db143fa88501cbf8089e6dbaea4a
shaxweb <igrayunagitare@gmail.com>
2025-07-05 22:11:30 +0500

    update urls.py

--- SomeStore/main/urls.py
+++ SomeStore/main/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.StartAPi.as_view()),
     path('get_all_datas/', views.GetAllDatasApi.as_view()),
     path('get_data/', views.GetDataApi.as_view()),
-    path('get_products/', views.GetProductsApi()),
+    path('get_products/', views.GetProductsApi.as_view()),
     path('register/', views.RegisterUserApi.as_view()),
     path('login/', views.LoginUserApi.as_view()),
     path('auth/', views.AuthUserApi.as_view()),
