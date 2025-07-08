fc550a6987ab0837939718029709ab49be5ead22
shaxweb <igrayunagitare@gmail.com>
2025-07-08 10:57:34 +0500

    update settings.py

--- SomeStore/SomeStore/settings.py
+++ SomeStore/SomeStore/settings.py
@@ -30,14 +30,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['somestorebackend.onrender.com']
 ALLOWED_SLASH = True
-#CORS_ORIGIN_ALLOW_ALL = True
-#CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000", "http://localhost", "https://localhost"]
-#CORS_ALLOW_METHODS = ["GET", "POST", "OPTIONS"]
-#CORS_ALLOW_HEADERS = ["content-type", "authorization", "x-requested-with"]
-#CORS_ALLOWED_HOSTS = ["localhost", "somaestorebackend"]
-# new
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = False
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
